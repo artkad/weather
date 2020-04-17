a7ad208a89332173c669ae016006ea535d3ecd60
artkad <kadkin.artem@gmail.com>
2020-04-17 14:35:42 +0500

    get temp

--- public/app.js
+++ public/app.js
@@ -1,24 +1,43 @@
-//const http = require('http');
-//const server = http.createServer((req, res) => {
-//  if(req.url === '/'){
-//    res.write('hello man art');
-//    res.end();
-//  }
-//});
-//
-//server.listen(3000);
-//console.log('server 3000...');
-//const API = http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3b1ceba16bf4976a7316ae5db42cf48f;
-//
+//const API =`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3b1ceba16bf4976a7316ae5db42cf48f`; 
 const API ='/api'
+//const list = {};
+class SwapiService {
+  async getResource(url) {
+    const res = await fetch(url);
+    return await res.json();
+  }
 
-fetch(API)
-  .then((response => {
-    return response.json();
-  }))
-  .catch(error => {
-    console.log(error);
-  })
+  async getWeather() {
+    const res = await this.getResource(API);
+    return res;
+  }
+}
+const swapi = new SwapiService();
+
+const findTemp = function(temp) {
+    let dateT = new Date();
+    let curentTime = dateT.toString().match(/(?<=\s)\w{2}(?=:)/g)
+    let searchTime = '';
+    let searchTemp = '';
+    temp.forEach((el) => {
+        searchTime = el.dt_txt.match(/(?<=\s)\w{2}(?=:)/g)
+        if(curentTime[0] == searchTime[0]) {
+          searchTemp = +el.main.temp; 
+            console.log(searchTemp)
+        } 
+    });
+    return searchTemp
+}
+
+swapi.getWeather()
   .then((data) => {
-    console.log(data);
-});
+    console.log(data)
+  const {id, name} = data.city;
+  const temp = data.list;
+     console.log(findTemp(temp))
+  const html = `
+  <div class='title-city'>${ name }</div>
+  <div class='temp-clock'>${ (findTemp(temp) - 32)/ 1.8 }</div>`;
+  document.querySelector('.container').insertAdjacentHTML('beforeend', html);
+  });
+
